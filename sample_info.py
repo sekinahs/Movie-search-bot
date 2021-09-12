193df06fb4612c18b4a327201362971eb1060e3f
Megastarbot <89236767+Megastarbot@users.noreply.github.com>
2021-09-12 12:30:38 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -24,7 +24,7 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 # Messages
 START_MSG = """
 **Hi, I'm Media Search bot**
-
+MalluSearchBot use cheyy too 
 Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
 
